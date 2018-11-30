24018720c433dc546601634ab3b2f17d4b9df54f
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-29 22:35:43 -0500

    Going to instal laravel into this one.

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
+<?ph
+
+//This is going to be a laravel app! 
+
+
+
+
+
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Chapman's Coffee House</title>
 </head>
 <body>
     This page is going to include:
     <ul>
+        <li>Laravel Instillation.</li>
         <li>Brand landing-about page</li>
         <li>Branded Shopping cart</li>
         <li>Branded Employee page</li>
